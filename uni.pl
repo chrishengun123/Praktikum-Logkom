0f2d7bce0ee7412c5319b016a30eff502257ad13
chrishengun123 <chicogunawan@gmail.com>
2026-05-12 04:31:29 +0700

    conditional formatting

--- uni.pl
+++ uni.pl
@@ -141,7 +141,10 @@ playCard(I) :-
     [LastCard|_] = SubDiscardPile,
     [LastColor, LastType] = LastCard,
     (
-    ((SubDiscardPile == []; Color == "black"; LastColor = "black"; Color == LastColor; Type == LastType; \+ (Type == "wild", LastType == "wild"); \+ (Type == "wild_draw_4", LastType == "wild_draw_4")) ->
+    (((SubDiscardPile == []; Color == "black"; LastColor = "black";
+        Color == LastColor; Type == LastType),
+        \+ (Type == "wild", LastType == "wild"),
+        \+ (Type == "wild_draw_4", LastType == "wild_draw_4")) ->
         delete_at(Cards, I, NewCards),
         DiscardPile = [Card | SubDiscardPile],
         write_file(File, NewCards),
