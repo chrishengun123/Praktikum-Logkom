bd22d175ba6cce069311341c2789c65426e419d6
chrishengun123 <chicogunawan@gmail.com>
2026-05-12 10:55:46 +0700

    fix: write_file

--- file_read_write.pl
+++ file_read_write.pl
@@ -47,7 +47,7 @@ read_file(File, Output) :-
 
 write_file(File, Input) :-
     (Input == []);
-    Input = [Card | SubInput].
+    Input = [Card | SubInput],
     open(File, write, Stream),
     format(Stream, "~w-~w", [Card]),
     close(Stream),
