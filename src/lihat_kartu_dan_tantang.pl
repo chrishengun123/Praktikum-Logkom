d4be7e351c65268d6dd03cf8a4f0e8befe2770f6
nateuplord <natanaelcfs1234@gmail.com>
2026-05-19 14:15:27 +0700

    feat/fix: added tantang, renamed lihat_kartu.pl
    lihat_kartu was renamed to lihat_kartu_dan_tantang because now it also contains tantang predicate

---
+++ src/lihat_kartu_dan_tantang.pl
@@ -0,0 +1,58 @@
+peek :- lihatKartu. % js to make it faster
+/* THIS ONE WORKS, BUT IS NOT EXACTLY THE SAME AS THE EXAMPLE IN THE SPEK*/ 
+lihatKartu :-
+    (\+ started -> fail);
+    currentPlayer(Player),
+%    format("~w", [Cardfile]), for debug purposes
+    read_file(Player, Cards),
+    get_length(Cards, Length),
+    format("~nBerikut kartu yang Anda miliki: ~n", []),
+    printCards(1, Length, Cards).
+
+printCards(_, _, []) :- !.
+printCards(Index, Length, [Card | Rest]) :-
+    Index =< Length,
+    Index1 is Index + 1,
+    format("~d. ~w-~w~n", [Index | Card]),
+    printCards(Index1, Length, Rest).
+
+tantang :-
+    (\+ started -> fail);
+    % get the last player that played
+    format("Tantangan dilakukan!~n", []),
+    currentPlayer(Player),
+    turnOrder(Order),
+    get_length(Order, PlayerAmount),
+    get_index(Order, Player, Turn),
+    LastTurn is (Turn - 1) mod PlayerAmount,
+    get_element(Order, LastTurn, LastPlayer),
+    % read the last player's cards
+    format("Memeriksa kartu ~w...", [LastPlayer])
+    read_file(LastPlayer, LastPlayerCards), 
+    % get discard pile
+    read_file('discard.txt', Discard),
+    get_element(Discard, 1, LastCard),
+    % get lastcard color and number/type
+    [LastColor, LastType] = LastCard,
+    hasSameColorOrType(Player, LastPlayer, LastPlayerCards, LastColor, LastType).
+% logic: tantangan berhasil
+hasSameColorOrType(Player, LastPlayer, LastPlayerCards, LastColor, LastType) :- 
+    (hasColor(LastPlayerCards, LastColor) ; hasType(LastPlayerCards, LastType)) -> 
+    (format("Tantangan berhasil! ~w mendapatkan 4 kartu acak.", [LastPlayer]),
+    switchPlayer(Player, LastPlayer),
+    ambilKartu,
+    ambilKartu,
+    ambilKartu,
+    ambilKartu,
+    switchPlayer(LastPlayer, Player); true).
+% tantangan gagal
+hasSameColorOrType(Player, LastPlayer, LastPlayerCards, LastColor, LastType) :- 
+    (\+(hasColor(LastPlayerCards, LastColor)), \+(hasType(LastPlayerCards, LastType))) -> 
+    (format("Tantangan gagal! ~w mendapatkan 6 kartu acak.", [Player]),
+    ambilKartu,
+    ambilKartu,
+    ambilKartu,
+    ambilKartu,
+    ambilKartu,
+    ambilKartu; true).
+
--- src/uni.pl
+++ src/uni.pl
@@ -1,4 +1,4 @@
-:- initialization([random_card, file_read_write, lihat_kartu]).
+:- initialization([random_card, file_read_write, lihat_kartu_dan_tantang]).
 % True when the game has started.
 :- dynamic(started/0).
 % The current player.
